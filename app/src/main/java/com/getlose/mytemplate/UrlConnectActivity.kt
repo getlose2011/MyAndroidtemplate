13b80aa2ce2378e61d6f9cc4dd73d3461028ffe1
jenhsiang <getlose2011@gmail.com>
2021-08-17 10:09:46 +0800

    網路連線與 JSON 資料解析實務改用coroutines

--- app/src/main/java/com/getlose/mytemplate/UrlConnectActivity.kt
+++ app/src/main/java/com/getlose/mytemplate/UrlConnectActivity.kt
@@ -5,38 +5,68 @@ import android.os.Bundle
 import android.util.Log
 import com.getlose.mytemplate.Model.SnookerEvent
 import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.json.JSONArray
 import java.net.URL
+import kotlin.coroutines.CoroutineContext
 
-class UrlConnectActivity : AppCompatActivity() {
+class UrlConnectActivity : AppCompatActivity(),CoroutineScope{
 
     val TAG = UrlConnectActivity::class.java.simpleName
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_url_connect)
+        job = Job()
 
-        //網路秏時工作要在非UI執行緒執行
-        Thread{
+        launch {
             //開啓網路權限<uses-permission android:name="android.permission.INTERNET" />
             //在 AndroidManifext.xml加上android:usesCleartextTraffic="true",
             //即可訪問http網站
             val data = URL("http://api.snooker.org/?t=5&s=2020").readText()
             val json = JSONArray(data)
             Log.d(TAG, "array length: ${json.length()}")
-            for (i in 0..json.length()-1){
+            for (i in 0..json.length() - 1) {
                 //還未用gson外掛前
                 //val obj = json.getJSONObject(i)
                 //Log.d(TAG, "obj: ${obj.getInt("ID")}")
 
                 //gson
-                val result = Gson().fromJson(data,SnookerEvent::class.java)
+                val result = Gson().fromJson(data, SnookerEvent::class.java)
                 result.forEach {
                     Log.d(TAG, "snooker data=> ${it}")
                 }
             }
-            //Log.d(TAG, "URL: $data")
-        }.start()
+        }
+        //網路秏時工作要在非UI執行緒執行
+//        Thread{
+//            //開啓網路權限<uses-permission android:name="android.permission.INTERNET" />
+//            //在 AndroidManifext.xml加上android:usesCleartextTraffic="true",
+//            //即可訪問http網站
+//            val data = URL("http://api.snooker.org/?t=5&s=2020").readText()
+//            val json = JSONArray(data)
+//            Log.d(TAG, "array length: ${json.length()}")
+//            for (i in 0..json.length()-1){
+//                //還未用gson外掛前
+//                //val obj = json.getJSONObject(i)
+//                //Log.d(TAG, "obj: ${obj.getInt("ID")}")
+//
+//                //gson
+//                val result = Gson().fromJson(data,SnookerEvent::class.java)
+//                result.forEach {
+//                    Log.d(TAG, "snooker data=> ${it}")
+//                }
+//            }
+//            //Log.d(TAG, "URL: $data")
+//        }.start()
 
     }
+
 }
